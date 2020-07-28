adc5545e186faeda921405b0d764f374de682929
Kellina Oliveira <kellina@gmail.com>
2020-07-28 19:47:17 -0300

    Adicionado carousel, categorias e footer

---
+++ src/pages/Home/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Menu from '../../componentes/Menu'
+import BannerMain from '../../componentes/BannerMain'
+import Carousel from '../../componentes/Carousel'
+import Footer from '../../componentes/Footer'
+import dadosIniciais from '../../data/dados_iniciais.json'
+
+function Home() {
+  return (
+    <div style={{background: "#141414"}}>
+      <Menu/>
+      
+      <BannerMain
+        videoTitle={dadosIniciais.mainVideo.titulo}
+        url={dadosIniciais.mainVideo.url}
+        videoDescription={dadosIniciais.mainVideo.description}
+      />
+
+      {dadosIniciais.categorias.map((categoria) => <Carousel category={categoria}/>)}
+
+      <Footer/>
+    </div>
+  );
+}
+
+export default Home;
